d70cb6f25799ad2aa8b9d51918641120a468c0e9
lohith14 <lohith14@gmail.com>
2022-03-25 12:21:08 +0530

    Create prg_range_xrange.py
    Initial commit

---
+++ prg_range_xrange.py
@@ -0,0 +1,12 @@
+print ("Program Xrange and range")
+
+lt = ['a', 'b', 'c', 1,5,3,8]
+print ("list is:", lt)
+
+for var in lt:
+    print (var)
+
+print ("range list is:", range(6))
+
+for rval in range(2,60,5):
+    print (rval)
