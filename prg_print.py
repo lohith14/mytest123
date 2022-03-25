36e1db7f2cf99418266d27d30a4b507911865574
lohith14 <lohith14@gmail.com>
2022-03-25 12:21:38 +0530

    Update prg_print.py

--- prg_print.py
+++ prg_print.py
@@ -1,12 +1,2 @@
-print ("Program Xrange and range")
+print ("First program")
 
-lt = ['a', 'b', 'c', 1,5,3,8]
-print ("list is:", lt)
-
-for var in lt:
-    print (var)
-
-print ("range list is:", range(6))
-
-for rval in range(2,60,5):
-    print (rval)
